41ee44b48560d1a754b3ad3df50dc65dadb61841
David Samuel Sitorus <sammysitorus23@gmail.com>
2026-07-28 18:17:46 +0700

    chore(models): add jabatan in class DosenRecord

--- src/models/model-dosen.js
+++ src/models/model-dosen.js
@@ -1,38 +1,39 @@
 export class DosenRecord {
-    constructor(data) {
-        this.univ = data.univ || null;
-        this.fullname = data.fullname || 'Unknown';
-        this.nama = data.nama || null;
-        this.fakultas = data.fakultas || 'Unassigned';
-        this.prodi = data.prodi || null;
-        this.foto = data.foto || null;
-        this.link = data.link || null;
-        this.email = data.email || null;
-        
-        this.gelarProf = data.gelarProf ? data.gelarProf.trim() : null;
-        this.gelarDepan = data.gelarDepan ? data.gelarDepan.trim() : null;
-        this.gelarBelakang = data.gelarBelakang ? data.gelarBelakang.trim() : null;
-        
-        this.id = this.generateId();
-    }
+  constructor(data) {
+    this.univ = data.univ || null;
+    this.fullname = data.fullname || "Unknown";
+    this.nama = data.nama || null;
+    this.fakultas = data.fakultas || "Unassigned";
+    this.prodi = data.prodi || null;
+    this.foto = data.foto || null;
+    this.link = data.link || null;
+    this.email = data.email || null;
+    this.jabatan = data.jabatan || null;
 
-    generateId() {
-        const safeName = this.nama 
-            ? this.nama.replace(/[^a-zA-Z]/g, '').toLowerCase() 
-            : 'unknown';
+    this.gelarProf = data.gelarProf ? data.gelarProf.trim() : null;
+    this.gelarDepan = data.gelarDepan ? data.gelarDepan.trim() : null;
+    this.gelarBelakang = data.gelarBelakang ? data.gelarBelakang.trim() : null;
 
-        const safeFakultas = this.fakultas
-            ? this.fakultas.replace(/[^a-zA-Z]/g, '').toLowerCase()
-            : 'unassigned';
-            
-        const safeProdi = this.prodi
-            ? this.prodi.replace(/[^a-zA-Z]/g, '').toLowerCase()
-            : 'unassigned';
+    this.id = this.generateId();
+  }
 
-        const univ = this.univ
-            ? this.univ.replace(/[^a-zA-Z]/g, '').toLowerCase()
-            : 'unknown'
+  generateId() {
+    const safeName = this.nama
+      ? this.nama.replace(/[^a-zA-Z]/g, "").toLowerCase()
+      : "unknown";
 
-        return `dosen:${safeName}_${safeProdi}_${safeFakultas}_${univ}`;
-    }
+    const safeFakultas = this.fakultas
+      ? this.fakultas.replace(/[^a-zA-Z]/g, "").toLowerCase()
+      : "unassigned";
+
+    const safeProdi = this.prodi
+      ? this.prodi.replace(/[^a-zA-Z]/g, "").toLowerCase()
+      : "unassigned";
+
+    const univ = this.univ
+      ? this.univ.replace(/[^a-zA-Z]/g, "").toLowerCase()
+      : "unknown";
+
+    return `dosen:${safeName}_${safeProdi}_${safeFakultas}_${univ}`;
+  }
 }
